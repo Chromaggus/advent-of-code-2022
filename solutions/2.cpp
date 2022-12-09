93da533e1daf2de647376dcb0f917801c5922bd0
Harry Zhang <z5363493@ad.unsw.edu.au>
2022-12-09 17:38:27 +1100

    day 2

--- solutions/2.cpp
+++ solutions/2.cpp
@@ -8,10 +8,12 @@ int main() {
 
     while (std::getline(std::cin, line)) {
         char opponent_char = line[0];
-        char your_char = line[2];
-
         int opponent_play = opponent_char - 'A';
+
+        char your_char = line[2];
         int your_play = your_char - 'X';
+
+        /*  PART 1
 
         // Get score based on what you played
         total_score += your_play + 1;
@@ -22,10 +24,16 @@ int main() {
         } else if (your_play == (opponent_play + 1) % 3) {
             total_score += 6;
         }
+        */
 
-        // std::cout << opponent_char << " " << your_char << " " << total_score << std::endl;
+        // Get score based on if you want to win
+        total_score += (your_play - 1) * 3 + 3;
+
+        // Get score based on what you played
+        total_score += ((opponent_play + your_play - 1) % 3 + 3) % 3 + 1;
+
+        std::cout << opponent_char << " " << your_char << " " << total_score << std::endl;
     }
 
     std::cout << "Total score: " << total_score << std::endl;
-
 }
